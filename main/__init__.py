f22178a260b22f304fb835e7f1dd046605188756
xd3s507 <147215673+xd3s507@users.noreply.github.com>
2023-10-07 11:28:59 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,16 +12,16 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("21037154", default=None, cast=int)
+API_HASH = config("b70bf9ef5b87f66bdbd39968daf33dcf", default=None)
+BOT_TOKEN = config("6500428452:AAETFvY6cUKoze_qMf34C_xzJ5QhnPRQudo", default=None)
+SESSION = config("6500428452", default=None)
+FORCESUB = config("Save_alshehri_bot", default=None)
+AUTH = config("xn_ik", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("Save_alshehri_bot", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
