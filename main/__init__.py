7bfd1e60349ef740270c46ddc4c795895eb9789f
xd3s507 <147215673+xd3s507@users.noreply.github.com>
2023-10-16 00:25:28 +0300

    Add files via upload

--- main/__init__.py
+++ main/__init__.py
@@ -15,13 +15,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("21037154", default=None, cast=int)
 API_HASH = config("b70bf9ef5b87f66bdbd39968daf33dcf", default=None)
 BOT_TOKEN = config("6500428452:AAETFvY6cUKoze_qMf34C_xzJ5QhnPRQudo", default=None)
-SESSION = config("6500428452", default=None)
-FORCESUB = config("Save_alshehri_bot", default=None)
-AUTH = config("xn_ik", default=None, cast=int)
+SESSION = config("MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR", default=None)
+FORCESUB = config("xn_ik", default=None)
+AUTH = config("Save_alshehri_bot", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("Save_alshehri_bot", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
