660ed407b74e6d134b16eeb619c420aec97e7363
xd3s507 <147215673+xd3s507@users.noreply.github.com>
2023-10-16 00:58:00 +0300

    Add files via upload

--- main/__init__.py
+++ main/__init__.py
@@ -16,8 +16,8 @@ API_ID = config("21037154", default=None, cast=int)
 API_HASH = config("b70bf9ef5b87f66bdbd39968daf33dcf", default=None)
 BOT_TOKEN = config("6500428452:AAETFvY6cUKoze_qMf34C_xzJ5QhnPRQudo", default=None)
 SESSION = config("MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR", default=None)
-FORCESUB = config("xn_ik", default=None)
-AUTH = config("Save_alshehri_bot", default=None, cast=int)
+FORCESUB = config("Save_alshehri_bot", default=None)
+AUTH = config("6589150729", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
